2a6181cfa18110d2813e9c082af5afe0b920876d
swg\jka <jka@aptean.com>
2025-11-12 09:25:02 +0530

    Removed UnUsed Imports

--- app/src/main/java/com/example/warehouse_go/ReceiptDetailScreen.kt
+++ app/src/main/java/com/example/warehouse_go/ReceiptDetailScreen.kt
@@ -5,19 +5,15 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.example.warehouse_go.models.ReceiptDetails
-import com.example.warehouse_go.models.ReceiptLine
 import com.example.warehouse_go.models.receiptDetails
 
 @Composable
